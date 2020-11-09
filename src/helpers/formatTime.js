83a8965d0e2e9b07d2f8f9df1e7aa5f9bc1b8ab9
ankitOneNineNineNine <pradhanankit12@gmail.com>
2020-11-09 18:21:32 +0545

    Updates

--- src/helpers/formatTime.js
+++
@@ -1,9 +0,0 @@
-export default function formatTime(seconds) {
-    var days = Math.floor(seconds / (24 * 60 * 60));
-    seconds -= days * (24 * 60 * 60);
-    var hours = Math.floor(seconds / (60 * 60));
-    seconds -= hours * (60 * 60);
-    var minutes = Math.floor(seconds / 60);
-    seconds -= minutes * 60;
-    return minutes + ":" + parseInt(seconds);
-}
